127ae0c47cf944980efb89fdbd5408fd836f7ddd
Alex King <kingalex2002@gmail.com>
2022-06-03 12:57:01 -0700

    finished game

--- FinalProject.py
+++ FinalProject.py
@@ -2,22 +2,22 @@
 import time
 
 
-WIDTH = 2
-HEIGHT = 2
+WIDTH = 10
+HEIGHT = 15
 
-FLAG_CHAR = "P"
+FLAG_CHAR = "◘"
 FULL_CHAR = "■"
 MINE_CHAR = "*"
 EMPTY_CHAR = " "
 
 def generate_mines(width, height):
-    board = []
+    mines = []
     for x in range(width):
-        board.append([])
+        mines.append([])
         for y in range(height):
-            rand = random.randrange(5)
-            board[x].append(True if rand == 0 else False)
-    return board
+            rand = random.randrange(6)
+            mines[x].append(True if rand == 0 else False)
+    return mines
 
 def generate_board(width, height):
     board = []
@@ -28,31 +28,35 @@ def generate_board(width, height):
     return board
 
 #debug only
-def print_mines(board):
-    for y in range(len(board[0])):
-        for x in range(len(board)):
-            print(MINE_CHAR if board[x][y] else EMPTY_CHAR, end="")
+def print_mines(mines):
+    for y in range(len(mines[0])):
+        for x in range(len(mines)):
+            print(MINE_CHAR if mines[x][y] else EMPTY_CHAR, end="")
         print()
 
 def print_board(board):
-    dash_string = "  ---"
+    dash_string = "  -----"
     for i in range(len(board)):
         dash_string += "--"
     print(dash_string)
-    string = " |   "
+    string = " |     "
     for i in range(len(board)):
         string += chr(ord("a") + i) + " "
-    print(string)
+    print(string + "|")
+    string = " |     "
+    for i in range(len(board)):
+        string += "--"
+    print(string + "|")
     for y in range(len(board[0])):
         print(" |", end="")
-        if(y < 10): 
+        if(y < 9): 
             #print additional space for single digit numbers
             print(" ", end = "")
-        print(str(y + 1) + " ", end="")
+        print(str(y + 1) + " | ", end="")
         
         for x in range(len(board)):
             print(board[x][y], end=" ")
-        print()
+        print("|")
     print(dash_string)
 
 def check_win(board, mines):
@@ -64,13 +68,44 @@ def check_win(board, mines):
                 return False
     return True
 
+#This function calculates the amount of mines in a 3x3 area
+def calc_mines(mines, x, y):
+    mine_count = 0
+    for xi in range(x - 1, x + 2):
+        for yi in range(y - 1, y + 2):
+            #not middle square
+            not_middle_square = not(xi == x and yi == y)
+            on_board = 0 <= xi < len(mines) and 0 <= yi < len(mines[0])
+            if(on_board and not_middle_square):
+                if(mines[xi][yi]):
+                    mine_count += 1
+
+    return mine_count
+        
+
+#This function recursively clears out space until it reaches mines and also adds numbers saying how many mines are nearby (blank if zero)
 def hit_zero(board, mines, x, y):
-
-
+    mine_count = calc_mines(mines, x, y)
+    if(mine_count == 0):
+        board[x][y] = EMPTY_CHAR
+        for xi in range(x - 1, x + 2):
+            for yi in range(y - 1, y + 2):
+                on_board = 0 <= xi < len(mines) and 0 <= yi < len(mines[0])
+                if(not on_board):
+                    continue
+                not_middle_square = not(xi == x and yi == y)
+                not_empty = board[xi][yi] == FULL_CHAR or board[xi][yi] == FLAG_CHAR
+                if(not_middle_square and not_empty):
+                    hit_zero(board, mines, xi, yi)
+    else:
+        board[x][y] = str(mine_count)
 
 mines = generate_mines(WIDTH, HEIGHT)
 board = generate_board(WIDTH, HEIGHT)
 
+#debug only
+#print_mines(mines)
+#print board
 print_board(board)
 
 #Start game
@@ -94,11 +129,16 @@ while(playing):
                 message = "You Lost."
                 playing = False
             else:
-                board[x][y] = EMPTY_CHAR
+                mine_count = calc_mines(mines, x, y)
+                if(mine_count == 0):
+                    hit_zero(board, mines, x, y)
+                else:
+                    board[x][y] = str(mine_count)
                 print_board(board)
                 if(check_win(board, mines)):
                     message = "You Won!"
                     playing = False;
+            print(message)
         else:
             print("Can't mine an empty square")
     elif(move == "f"):
